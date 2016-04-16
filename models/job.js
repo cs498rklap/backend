186e66c47f89fe5a5caf771fd4f8c487aed4a7d0
Emily Chao <elchao96@live.com>
2016-04-15 22:29:36 -0500

    schemas for the two objects

---
+++ models/job.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var JobSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    company: {type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+    link: {type: String},
+    deadline: {type: Date},
+    description: {type: String},
+    tags: {type: [String]}
+});
+
+module.exports = mongoose.model('Job', JobSchema);

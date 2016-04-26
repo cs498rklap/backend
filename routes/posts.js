cc98a3fa3877d40ebe7ea6d8ffb25cbbc22486a1
William Fazio <faziow14@users.noreply.github.com>
2016-04-25 20:27:31 -0500

    Pulled new changes into my branch

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@ var express = require('express');
 var Posts = require('./../models/post');
 var router = express.Router();
 
-// Can just do router.post('/') or get('/')
+// Can just do postsRoute.post('/') or get('/')
 var postsRoute = router.route('/');
 
 // Get all posts

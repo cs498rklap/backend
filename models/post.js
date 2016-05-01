4f97604022009cdb1e786b9d1946b3437c8701f4
Emily Chao <elchao96@live.com>
2016-05-01 18:33:28 -0500

    comments gone

--- models/post.js
+++ models/post.js
@@ -1,13 +1,5 @@
 var mongoose = require('mongoose');
 
-/*var CommentSchema = new mongoose.Schema({
-    username: {type: String, required: true},
-    text: {type: String, required: true},
-    timestamp: {type: Date, default: Date.now}
-});
-
-var Comment = mongoose.model('Comment', CommentSchema);*/
-
 var PostSchema = new mongoose.Schema({
     title: {type: String, required: true},
     author: {type: String, required: true},
